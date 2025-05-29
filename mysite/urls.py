66b7573b0c91b257ef2bcf7211fb52111cbce580
sgarri4 <sgarri4@ilg.cat>
2025-05-29 15:48:49 +0200

    tutorial acabat

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,8 +23,9 @@ urlpatterns = [
 
 from django.contrib import admin
 from django.urls import include, path
-
+from polls.views import index
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("", index),
 ]
